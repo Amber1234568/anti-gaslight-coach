296f37a2ec4da098e4a1b704093f57e5cbec6ecb
shaoji11 <jingcheng.shao@mail.utoronto.ca>
2023-01-21 15:02:01 -0500

    user activity

--- activity/communication.py
+++ activity/communication.py
@@ -1,49 +1,6 @@
 import cohere
 from cohere.classify import Example
 
-def get_start_class(response: str) -> str:
-    examples = [Example("I don\'t care", "negative"),
-                Example("go to hell", "negative"),
-                Example("don\'t be a coward", "negative"),
-                Example("you petty little bitch", "negative"),
-                Example("why so rude", "neutral"),
-                Example("But that\'s not my fault", "neutral"),
-                Example("where\'s the classroom", "neutral"),
-                Example("thank you very much", "positive"),
-                Example("I really appreciate it", "positive"),
-                Example("I like your dress", "positive"),
-                Example("you did good", "positive"),
-                Example("The order came 5 days early", "positive"),
-                Example("The item exceeded my expectations", "positive"),
-                Example("I ordered more for my friends", "positive"),
-                Example("I would buy this again", "positive"),
-                Example("I would recommend this to others", "positive"),
-                Example("The package was damaged", "negative"),
-                Example("The order is 5 days late", "negative"),
-                Example("The order was incorrect", "negative"),
-                Example("I want to return my item", "negative"),
-                Example("The item\'s material feels low quality", "negative"),
-                Example("The product was okay", "neutral"),
-                Example("I received five items in total", "neutral"),
-                Example("I bought it from the website", "neutral"),
-                Example("I used the product this morning", "neutral"),
-                Example("The product arrived yesterday", "neutral")]
-    co = cohere.Client('mDgdZhuZohzzJnVmOnygreJzaxHyvUD4MdPcEoOq')
-
-    #print(
-    #    f"OK, you said \" {response} \"  Let's see if this is a good response.")
-
-    response_class = co.classify(
-        model='large',
-        inputs=[response],
-        examples=examples,
-    )
-
-    sentence = str(response_class.classifications[0])
-    result = sentence.index('prediction: ')
-    result_class = sentence[result + 13: result + 21]
-    return result_class
-
 def get_class(response: str) -> str:
     examples = [Example("I don\'t care", "negative"),
                 Example("go to hell", "negative"),
@@ -72,9 +29,6 @@ def get_class(response: str) -> str:
                 Example("I used the product this morning", "neutral"),
                 Example("The product arrived yesterday", "neutral")]
     co = cohere.Client('mDgdZhuZohzzJnVmOnygreJzaxHyvUD4MdPcEoOq')
-
-    print(
-        f"OK, you said \" {response} \"  Let's see if this is a good response.")
 
     response_class = co.classify(
         model='large',
@@ -110,7 +64,7 @@ def reasonable_answer(class_start: str, class_response: str) -> str:
 if __name__ == '__main__':
     start_sentence = "You can’t even get that answer? You’re so dumb!"
     print("You can’t even get that answer? You’re so dumb!")
-    class_start = get_start_class(start_sentence)
+    class_start = get_class(start_sentence)
 
     response = input("Enter your response: ")
     result_class = get_class(response)
